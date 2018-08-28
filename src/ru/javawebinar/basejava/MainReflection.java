ec4e2bf3df7ae0fd3e9745decc8e2bac0cc57bbc
AZ <fast.optimus@gmail.com>
2018-08-28 20:22:15 +0300

    HW04 reflection solution

---
+++ src/ru/javawebinar/basejava/MainReflection.java
@@ -0,0 +1,26 @@
+package ru.javawebinar.basejava;
+
+import ru.javawebinar.basejava.model.Resume;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class MainReflection {
+
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        Resume r = new Resume();
+        Field field = r.getClass().getDeclaredFields()[0];
+        field.setAccessible(true);
+        System.out.println(field.getName());
+        System.out.println(field.get(r));
+        field.set(r, "new_uuid");
+        System.out.println(r);
+
+        // TODO : invoke r.toString via reflection
+        System.out.println("------------------");
+        Method resumeMethod = r.getClass().getDeclaredMethod("toString");
+        System.out.println("Invoke method toString() for r instance: " + resumeMethod.invoke(r));
+
+    }
+}
