a5fa98df48d7f4eca7d70ec819a1af873aecbeda
AZ <fast.optimus@gmail.com>
2018-11-30 17:49:27 +0300

    HW08 ex.2

---
+++ src/ru/javawebinar/basejava/MainFile.java
@@ -0,0 +1,57 @@
+package ru.javawebinar.basejava;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class MainFile {
+
+    public static void main(String[] args) throws IOException {
+  /*      File file = new File(".");
+        System.out.println(file.getCanonicalPath());
+
+        File dir = new File(".\\src\\ru\\javawebinar\\basejava");
+        System.out.println(dir.getCanonicalPath());
+
+        System.out.println(dir.isDirectory());
+        if (dir.list() != null) {
+            for (String name : Objects.requireNonNull(dir.list())) {
+                System.out.println(name);
+            }
+        }*/
+
+        String filePath = ".\\.gitignore";
+        System.out.println(new File(filePath).exists());
+        FileInputStream fis = new FileInputStream(filePath);
+        System.out.println(fis.read());
+
+        printDirectory(".");
+
+    }
+
+    public static void printDirectory(String dirPath)  {
+        File dir = new File(dirPath);
+
+        File[] files = dir.listFiles();
+        if (files == null) {
+            throw new RuntimeException("This is not directory");
+        }
+
+        for (File f : files) {
+
+            String canonicalPath = null;
+            try {
+                canonicalPath = f.getCanonicalPath();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if (f.isDirectory()) {
+                printDirectory(canonicalPath);
+            } else {
+                System.out.println(canonicalPath);
+            }
+        }
+
+    }
+}
