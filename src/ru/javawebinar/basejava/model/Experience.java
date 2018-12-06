99093b1071e1c3fc25fb04bd2f9ea4bda03bf4e7
AZ <fast.optimus@gmail.com>
2018-12-06 03:04:33 +0300

    HW08 refactoring final

--- src/ru/javawebinar/basejava/model/Experience.java
+++ src/ru/javawebinar/basejava/model/Experience.java
@@ -2,6 +2,7 @@ package ru.javawebinar.basejava.model;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Objects;
 
 public class Experience extends AbstractSection {
     private final LocalDate startDate;
@@ -25,5 +26,22 @@ public class Experience extends AbstractSection {
                 title +
                 "\n" +
                 description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Experience that = (Experience) o;
+        return Objects.equals(startDate, that.startDate) &&
+                Objects.equals(endDate, that.endDate) &&
+                Objects.equals(title, that.title) &&
+                Objects.equals(description, that.description) &&
+                Objects.equals(dateTimeFormatter, that.dateTimeFormatter);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(startDate, endDate, title, description, dateTimeFormatter);
     }
 }
