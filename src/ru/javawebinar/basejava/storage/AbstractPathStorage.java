be2b0909c0c4a41227d73e7f44537b04a775b707
AZ <fast.optimus@gmail.com>
2018-12-13 01:20:23 +0300

    HW09 preparation 3

---
+++ src/ru/javawebinar/basejava/storage/AbstractPathStorage.java
@@ -0,0 +1,105 @@
+package ru.javawebinar.basejava.storage;
+
+import ru.javawebinar.basejava.exceptions.StorageException;
+import ru.javawebinar.basejava.model.Resume;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public abstract class AbstractPathStorage extends AbstractStorage<File> {
+    private File directory;
+
+    protected AbstractPathStorage(File directory) {
+        Objects.requireNonNull(directory, "directory must not be null");
+        if (!directory.isDirectory()) {
+            throw new IllegalArgumentException(directory.getAbsolutePath() + " is not directory");
+        }
+        if (!directory.canRead() || !directory.canWrite()) {
+            throw new IllegalArgumentException(directory.getAbsolutePath() + " is not readable/writable");
+        }
+        this.directory = directory;
+    }
+
+    @Override
+    protected List<Resume> getAllFromStorage() {
+        List<Resume> resumeList = new ArrayList<>();
+        File[] files = directory.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                try {
+                    resumeList.add(doRead(new BufferedInputStream(new FileInputStream(file))));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return resumeList;
+    }
+
+    @Override
+    protected boolean isExist(File file) {
+        return file.exists();
+    }
+
+    @Override
+    protected void saveElement(Resume resume, File file) {
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            throw new StorageException("IO error", file.getName(), e);
+        }
+        doUpdate(file, resume);
+    }
+
+    protected abstract void doWrite(Resume resume, OutputStream os) throws IOException;
+
+    @Override
+    protected void doUpdate(File file, Resume resume) {
+        try {
+            doWrite(resume, new BufferedOutputStream(new FileOutputStream(file)));
+        } catch (IOException e) {
+            throw new StorageException("IO error before updating", file.getName(), e);
+        }
+    }
+
+    @Override
+    protected void removeElement(File file) {
+        if (!file.delete()) {
+            throw new StorageException("File delete error", file.getName());
+        }
+    }
+
+    @Override
+    protected File getSearchKey(String file) {
+        return new File(directory, file);
+    }
+
+    protected abstract Resume doRead(InputStream is) throws IOException;
+
+    @Override
+    protected Resume doGet(File file) {
+        try {
+            return doRead(new BufferedInputStream(new FileInputStream(file)));
+        } catch (IOException e) {
+            throw new StorageException("File read error", file.getName(), e);
+        }
+    }
+
+    @Override
+    public void clear() {
+
+        File[] files = directory.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                removeElement(file);
+            }
+        }
+    }
+
+    @Override
+    public int size() {
+        return Objects.requireNonNull(directory.listFiles()).length;
+    }
+}
